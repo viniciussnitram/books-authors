a8cfcfd65f8e1739669086efb56635dd692851ff
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-02-20 22:09:39 -0300

    chore(path alias): add path alias to pass less folders to import something

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true
+  },
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src', 'app'),
