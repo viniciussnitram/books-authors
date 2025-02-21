a7737e285c3797566addbabb3e031c0a93d03e0f
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-02-21 10:06:51 -0300

    feat(types): adds typing to the books API data

---
+++ src/app/entities/IBook.ts
@@ -0,0 +1,10 @@
+export interface IBook {
+  id: string;
+  title: string;
+  publish_year: number;
+  genre: string;
+  publisher: string;
+  pages: number;
+  synopsis: string;
+  autorId: string[];
+}
