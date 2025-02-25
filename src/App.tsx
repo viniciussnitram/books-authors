1ef79b395da09720696153ed4af29d2161b9bab2
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-02-25 14:11:48 -0300

    refactor(Component/App): refactor App Component to be the main Component and store the others

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
+import { useContext } from "react";
+import { IsBookSelectedContext } from "./app/contexts/IsBookSelectedContext";
+import { GlobalStyle, Main, Tab } from "./global";
+import Authors from "./views/components/ui/Authors/Authors";
+import Books from "./views/components/ui/Books/Books";
 
 function App() {
+  const isBookSelectedContext = useContext(IsBookSelectedContext);
+
+  if (!isBookSelectedContext) return <p>Carregando...</p>
+  const { isBookSelected, setIsBookSelected } = isBookSelectedContext;
+
   return (
-    <h1>Hello World!</h1>
-  )
+    <>
+      <GlobalStyle />
+
+      <Main>
+        <h1>Consultando {isBookSelected ? 'livros' : 'autores(as)'}</h1>
+
+        <Tab>
+          <button
+            className={!isBookSelected ? 'active' : ''}
+            onClick={() => setIsBookSelected(true)}
+          >
+            Livros
+          </button>
+
+          <button
+            className={isBookSelected ? 'active' : ''}
+            onClick={() => setIsBookSelected(false)}
+          >
+            Autores(as)
+          </button>
+        </Tab>
+
+        {isBookSelected ? (
+          <Books />
+        ) : (
+          <Authors />
+        )}
+      </Main>
+    </>
+  );
 }
 
-export default App
+export default App;
