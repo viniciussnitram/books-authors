cba9fb5ba7699bc1b2d2eb02d57ec43c88cb31e9
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-02-25 14:12:31 -0300

    refactor(main.tsx): refactor main.tsx to receive all the contexts

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { AuthorProvider } from './app/contexts/AuthorContext.tsx'
+import { BookProvider } from './app/contexts/BookContext.tsx'
+import { IsBookSelectedProvider } from './app/contexts/IsBookSelectedContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <IsBookSelectedProvider>
+      <BookProvider>
+        <AuthorProvider>
+          <App />
+        </AuthorProvider>
+      </BookProvider>
+    </IsBookSelectedProvider>
+  </StrictMode>
 )
